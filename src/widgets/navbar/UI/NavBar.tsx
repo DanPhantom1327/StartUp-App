3f3b6f1c0654d714e3e4d23f5251b2627a64a87b
Daniel <ludacar2799@gmail.com>
2025-10-20 19:53:11 -0500

    T-001-2: Agregar logo a la barra de navegación v 2.0

--- src/widgets/navbar/UI/NavBar.tsx
+++ src/widgets/navbar/UI/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
   return (
     <nav className="w-full bg-gray-900 text-white flex justify-between items-center px-6 py-4 shadow-md">
       <div className="flex items-center gap-2">
-        <Image src="startup-app/public/logo.jpeg" alt="logo" width={32} height={32} />
+        <Image src="/logo.jpeg" alt="logo" width={32} height={32} />
         <span className="text-xl font-bold">StartUp-App 🚀</span>
       </div>
       <div className="flex gap-4">
