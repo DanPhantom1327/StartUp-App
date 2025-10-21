a3a3deacc60df1229c0b1a189172112760dbd259
Daniel <ludacar2799@gmail.com>
2025-10-20 20:21:23 -0500

    Task 21: T-001-4: Realice un segundo layout con 4 cards donde al seleccionar el boton de CLIENTES lo envie a ellas, los clientes son: EXITO, OXXO, D1, LICORERA BUCAROS

---
+++ src/pages-startup/clientes/index.ts
@@ -0,0 +1,1 @@
+export { default as ClientesPage } from "./UI/Page";
--- src/pages-startup/home/UI/Page.tsx
+++ src/pages-startup/home/UI/Page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <section className="text-center mt-20 px-6">
-      <h1 className="text-4xl md:text-5xl font-extrabold text-gray-900 mb-4">
+      <h1 className="text-4xl md:text-5xl font-extrabold text-gray-400 mb-4">
         Bienvenido a <span className="text-green-500">StartUp App 🚀</span>
       </h1>
       <p className="text-lg md:text-xl text-gray-600 max-w-2xl mx-auto">
