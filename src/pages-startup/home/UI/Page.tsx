61580a105286a68de9e646096cdbd3bbb2cb9b53
Daniel <ludacar2799@gmail.com>
2025-10-20 19:57:54 -0500

    T-001-3: Crear layout de bienvenida en la página principal

---
+++ src/pages-startup/home/UI/Page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold mb-4">Bienvenido a StartUp App 🚀</h1>
+      <p className="text-lg text-gray-600">
+        Somos StartUp-App, una App hecha para ti pensando en tu futuro 🌎
+      </p>
+    </div>
+  );
+}
