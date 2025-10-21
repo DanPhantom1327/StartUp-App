4455f3d4f0d4c472c350d3f916dcf6b7c998b87f
Daniel <ludacar2799@gmail.com>
2025-10-20 20:04:26 -0500

    T-001-1: Crear barra navegacion superior en la app v 2.0

--- src/widgets/navbar/UI/NavBar.tsx
+++ src/widgets/navbar/UI/NavBar.tsx
@@ -1,20 +1,26 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
 
 export default function NavBar() {
   return (
     <nav className="w-full bg-gray-900 text-white flex justify-between items-center px-6 py-4 shadow-md">
-      <div className="flex items-center gap-2">
-        <Image src="/logo.jpeg" alt="logo" width={32} height={32} />
-        <span className="text-xl font-bold">StartUp-App 🚀</span>
-      </div>
+      <div className="text-xl font-bold">StartUp-App 🚀</div>
       <div className="flex gap-4">
-        <Link href="/">Inicio</Link>
-        <Link href="/about">Nosotros</Link>
-        <Link href="/contact">Contacto</Link>
+        <Link href="/" className="bg-green-400 hover:text-black-100 px-4 py-2 rounded-md">
+          Inicio
+        </Link>
+        <Link href="/clientes" className="bg-green-400 hover:text-black-100 px-4 py-2 rounded-md">
+          Nuestros clientes
+        </Link>
+        <Link
+          href="/login"
+          className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md transition"
+        >
+          Login
+        </Link>
       </div>
     </nav>
   );
 }
+
